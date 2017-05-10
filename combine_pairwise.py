522aeb4247a9d33e50aa4575ba36246de3184082
Zijian Zhang <zhangzijian0523@gmail.com>
2017-05-10 15:12:06 +0200

    refactorize code into functional programming, add merging order 'min_procrustes_err'

--- combine_pairwise.py
+++ combine_pairwise.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-    This script align different manifold and combine them together
-    using average along each dimension, both serially and dicotomically
-    merge.
+    This script combine different embeddings model according to their
+    vectors.
 
-    Usage: python combine.py
-                <order>
-                <with low rank alignment(Y/N)>
-                <combination strategy>
-                <folder/contain/sub-models>
-                <dir/combined-model>
-                <num of submodels>
+    Usage: python combine_pairwise.py
+                  <sub model dir>
+                  <num of sub models>
+                  <combination order>
+                  <combination strategy>
+                  <use lra?>
+                  <use normed vector?>
+                  <name of merged file>
 
     For order it can be:
-        serial: serial merge
-        dichoto: dicotomicall merge
+        sequential      : sequential merge
+        dichoto         : dicotomicall merge
+        min_linear_err  : everytime combine the two with minimum error w.r.t.
+                          orthogonal Procrustes error
 
     For combination strategy it can be:
-        avg: merge using average through each dimension
-        avgnorm: merge using average then normalization
-        hilbert: merge under hilbert space
+        vector_addition : merge using average through each dimension
+        linear_transform: merge using average then normalization
+        pca_transbase   : merge under hilbert space
 """
-
-
 import logging
 import os
 import sys
 import os.path
 import numpy as np
+import pickle
 
 from gensim import matutils
 from gensim.models.word2vec import Word2Vec
@@ -39,148 +40,127 @@ from glob import glob
 from config import LOG_FILE
 from lra import low_rank_align
 
+def load_vocab(vocab_file):
+    with open(vocab_file, 'r') as f:
+        vocab = [word.lower()[:-1] for word in f.readlines()]
+    return vocab
+        
+def vocab_intersect(v1, v2):
+    return v1.intersection(v2)
 
-def combine_avg(X, Y, times):
-    return (X*float(times)+Y)/float(times+1)
-
-def combine_avg_norm(X, Y, times):
-    Z = combine(X, Y, times)
-    Z = [matutils.unitvec(v) for v in Z]
-    return Z
-
-def combine_hilbert(X, Y):
-    return X + Y
-
-def linear_trans(X, Y):
-    s = Y.T.dot(X)
-    _, V = np.linalg.eig(s.T.dot(s))
-    _, W = np.linalg.eig(s.dot(s.T))
-    T = W.dot(V.T)
-    return Y.dot(T)
-
-class EmbeddingPairs(object):
-    def model_load(self, model_file_name):
-        model = Word2Vec.load(model_file_name)
-        if self.norm_init:
+def load_common_vectors(common_vocab, normed):
+    def _load_vectors(mname):
+        model = Word2Vec.load(mname)
+        if normed:
             model.init_sims()
-        return model
+        vectors = [model.wv[word] for word in common_vocab]
+        return np.array(vectors)
+    return _load_vectors
 
-    def get_base_vecs(self):
-        X = []
-        logger.info('start to retrieve embedding matrix')
-        X = [self.mx.wv[word] for word in self.vocab]
-        X = np.array(X)
-        return X
+def vocab_intersect(v1, v2):
+    return v1.intersection(v2)
 
-    def get_corresponders(self):
-        n_xsamples = self.X.shape[0]
-        Y = [self.my.wv[word] for word in self.vocab]
-        Y = np.array(Y)
-        return Y
+def procrustes_trans(X, Y):
+     s = Y.T.dot(X)
+     _, V = np.linalg.eig(s.T.dot(s))
+     _, W = np.linalg.eig(s.dot(s.T))
+     return W.dot(V.T)
 
-    def update_model(self, Z):
-        assert Z.shape[0] == len(self.vocab)
-        for idx, word in enumerate(self.vocab):
-            idx_org = self.mx.wv.vocab[word].index
-            if self.norm_init:
-                self.mx.wv.syn0norm[idx_org] = Z[idx]
-            else:
-                self.mx.wv.syn0[idx_org] = Z[idx]
+def procrustes_err(X, Y):
+    T = procrustes_trans(X, Y)
+    Ystar = Y.dot(T)
+    E = Ystar - X
+    return np.trace(E.T.dot(E))
     
-    def linear_trans(self):
-        self.mx = self.model_load(self.mns[0])
-        logger.info('Reference model loaded')
-        self.X = self.get_base_vecs()
-        Z = np.zeros(self.X.shape, dtype='f')
-        for sub_model_name in mns[1:]:
-            self.my = self.model_load(sub_model_name)
-            Y = self.get_corresponders()
-            logger.info('Marix to align loaded')
-            Xstar = linear_trans(self.X, Y)
-            Z += Xstar
-        Z += self.X
-        self.update_model(Z/len(mns))
+def combine(X, Y, strategy, lra=False):
+    def _vector_add(X, Y):
+        return X+Y
 
-    def serial_pairs_gen(self):
-        self.mx = self.model_load(self.mns[0])
-        logger.info('base model loaded')
-        logger.info('start to combine...')
-        self.X = self.get_base_vecs()
-        for sub_model_name in mns[1:]:
-            self.my = self.model_load(sub_model_name)
-            Y = self.get_corresponders()
-            logger.info('matrix to merge loaded')
-            self.X = yield [self.X, Y]
-            logger.info('merging finished')
-        self.update_model(self.X)
+    def _linear_trans(X, Y):
+        T = procrustes_trans(X, Y)
+        Ystar = Y.dot(T)
+        return _vector_add(X, Ystar)
 
-    def dichoto_pairs_gen(self):
-        limit = len(self.mns)
-        combine_queue = deque([str(i) for i in range(limit)])
-        idx_model = 0
+    def _pca_transbase(X, Y):
+        dim = X.shape[1]
+        V = np.hstack((X, Y))
+        R = V.T.dot(V)
+        _, evecs = np.linalg.eig(R)
+        evecs = evecs[:, :dim]
+        return V.dot(evecs)
+    
+    if lra:
+        X, Y = low_rank_align(X, Y, np.eye(X.shape[0]))
+    if strategy == 'linear_transform':
+        return _linear_trans(X, Y)
+    if strategy == 'vector_addition':
+        return _vector_add(X, Y)
+    if strategy == 'pca_transbase':
+        return _pca_transbase(X, Y)
 
-        def load_using_tag(tag):
-            name = (self.temp_models_dir +
-                    'article.txt.' +
-                    tag + '.txt.w2v')
-            model = Word2Vec.load(name)
-            if idx_model < self.num_org_models:
-                if self.norm_init:
-                    model.init_sims(replace=True)
-            else:
-                for filename in glob(name+'*'):
-                    os.remove(filename)
-            return model
+def merge(embeddings, order='sequential', strategy='avg', lra=False):
+    def merge_seq(embeddings):
+        embeddings = deque(embeddings)
+        while len(embeddings) > 1:
+            X = embeddings.popleft()
+            Y = embeddings.popleft()
+            Z = combine(X, Y, strategy, lra)
+            embeddings.appendleft(Z)
+        return embeddings.popleft()
 
+    def merge_dichoto(embeddings):
+        embeddings = deque(embeddings)
+        while len(embeddings) > 1:
+            X = embeddings.popleft()
+            Y = embeddings.popleft()
+            Z = combine(X, Y, strategy, lra)
+            embeddings.append(Z)
+        return embeddings.popleft()
+
+    def merge_min_procrustes_err(embeddings):
+        namelist = [i for i in range(len(embeddings))]
+        err = np.eye(len(embeddings)) * 32768.0
+        for i in namelist[:-1]:
+            for j in namelist[i+1:]:
+                err[i][j] = procrustes_err(embeddings[i], embeddings[j])
+                err[j][i] = err[i][j]
+        p = len(namelist)
         while True:
-            mx_tag = combine_queue.popleft()
-            self.mx = load_using_tag(mx_tag)
-            idx_model += 1
-
-            if len(combine_queue) == 0:
+            idx_i = namelist[0]
+            idx_j = namelist[0]
+            for _, i in enumerate(namelist[:-1]):
+                for j in namelist[_ + 1:]:
+                    if err[i][j] < err[idx_i][idx_j]:
+                        idx_i = i
+                        idx_j = j
+            Z = combine(embeddings[idx_i], embeddings[idx_j], strategy)
+            embeddings.append(Z)
+            namelist.remove(idx_i)
+            namelist.remove(idx_j)
+            if len(namelist) == 0:
                 break
+            namelist.append(p)
+            err = np.vstack((err, np.zeros((1, err.shape[1]))))
+            err = np.hstack((err, np.zeros((err.shape[0], 1))))
+            err[-1][-1] = 32768.0
+            for i in namelist[:-1]:
+                err[i][p] = procrustes_err(Z, embeddings[i])
+                err[p][i] = err[i][p]
+            p += 1
+        return embeddings[-1]
 
-            my_tag = combine_queue.popleft()
-            self.my = load_using_tag(my_tag)
-            idx_model += 1
-
-            self.X = self.get_base_vecs()
-            Y = self.get_corresponders()
-            Z = yield [self.X, Y]
-            self.update_model(Z)
-
-            combine_queue.append(mx_tag+my_tag)
-            combined_name = (self.temp_models_dir + 'article.txt.' +
-                             mx_tag + my_tag + '.txt.w2v')
-            self.mx.save(combined_name, ignore=['cum_table', 'table'])
-
-    def __init__(self, mns, vocab_file, order, temp_models_dir, norm_init):
-        self.mns = mns
-        self.num_org_models = len(self.mns)
-        self.temp_models_dir = temp_models_dir
-        with open(vocab_file, 'r') as vocab:
-            self.vocab = [word.lower()[:-1] for word in vocab.readlines()]
-        for modelname in self.mns:
-            mt = Word2Vec.load(modelname)
-            self.vocab = [word for word in self.vocab if word in mt.wv.vocab]
-        self.X = None
-        self.mx = None
-        self.my = None
-        if order == 'serial':
-            self.gen = self.serial_pairs_gen
-        if order == 'dichoto':
-            self.gen = self.dichoto_pairs_gen
-        self.norm_init = norm_init
-
-    def save_final(self, model_dir):
-        self.mx.save(model_dir, ignore=['table', 'cum_table'])
-
+    if order == 'sequential':
+        return merge_seq(embeddings)
+    elif order == 'dichoto':
+        return merge_dichoto(embeddings)
+    elif order == 'min_procrustes_err':
+        return merge_min_procrustes_err(embeddings)
+    
 
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
     global logger
     logger = logging.getLogger(program)
-
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
     logging.root.setLevel(level=logging.INFO)
     logger.info("running %s" % ' '.join(sys.argv))
@@ -189,62 +169,40 @@ if __name__ == '__main__':
         print globals()['__doc__'] % locals()
         sys.exit(1)
 
-    (order, use_lra, strategy, sub_models_dir,
-     new_model_name, num_sub_models) = sys.argv[1:7]
+    (sub_models_dir, num_sub_models, order,
+     strategy, lra, normed,
+     merged_file_name) = sys.argv[1:8]
     num_sub_models = int(num_sub_models)
-    if use_lra == 'Y':
-        use_lra = True
-    else:
-        use_lra = False
-
+    lra = (lra == 'Y')
+    normed = (normed == 'Y')
     log_file = LOG_FILE
+    benchmark_vocab_dir = 'vocab/vocab.txt'
     ftime = open(log_file, 'a+')
     ftime.write('time of alignment and combination with order:'
                 ' %s and strategy: %s\n' % (order, strategy))
     ftime.write('sub-models under %s\n' % sub_models_dir)
-    ftime.write('combined models saved as %s\n\n' % new_model_name)
+    ftime.write('combined models saved as %s\n\n' % merged_file_name)
 
-    etime = -ctime()
+    time_elapsed = -ctime()
 
-    mns = [sub_models_dir+'article.txt.'+str(i)+'.txt.w2v'
+    model_name_list = [sub_models_dir+'article.txt.'+str(i)+'.txt.w2v'
            for i in range(num_sub_models)]
-    ep = EmbeddingPairs(mns, 'vocab/vocab.txt', order, sub_models_dir, False)
-    if strategy == 'linear_trans':
-        ep.linear_trans()
-    else:
-        ep_gen = ep.gen()
-        
-        i = 0
-        Z = None
-
-    while True and not strategy == 'linear_trans':
-        try:
-            X, Y = ep_gen.send(Z)
-        except StopIteration:
-            break
-        dis0 = X-Y
-        dis0 = np.array([np.linalg.norm(dis0[j]) for j in range(len(dis0))])
-        i += 1
-        dis1 = Y-Ystar
-        dis1 = np.array([np.linalg.norm(dis1[j]) for j in range(len(dis1))])#.sum()
-        if use_lra:
-            X, Y = low_rank_align(X, Y, np.eye(X.shape[0]))
-        dis2 = Y-X
-        dis2 = np.array([np.linalg.norm(dis2[j]) for j in range(len(dis2))])#.sum()
-        print "origin diviation {}.\nsum of diviation using linear transformation is {}.\nsum of diviation using manifold alignment is {}.".format(dis0.sum(), dis1.sum(), dis2.sum())
-        if strategy == 'avg':
-            Z = combine_avg(X, Y, i)
-        if strategy == 'avgnorm':
-            Z = combine_avgnorm(X, Y, i)
-        if strategy == 'hilbert':
-            Z = combine_hilbert(X, Y)
-        if strategy == 'vadd':
-            Z = X + Y
+    vocabs = [set(Word2Vec.load(mname).wv.vocab) for mname in model_name_list]
+    init_vocab = set(load_vocab(benchmark_vocab_dir))
+    common_vocab = reduce(lambda x, y: x.intersection(y),
+                          vocabs,
+                          init_vocab)
+    common_vocab = list(common_vocab)
+    embeddings = map(load_common_vectors(common_vocab, normed), model_name_list)
     
-    ep.save_final(new_model_name)
+    merged_embeddings = merge(embeddings, order=order, strategy=strategy, lra=lra)
+   
+    with open(merged_file_name, 'w+') as ofile:
+        pickle.dump(dict(zip(common_vocab, merged_embeddings)), ofile)
 
-    etime += ctime()
-    ftime.write('combination time: %f sec\n\n' % etime)
+    time_elapsed += ctime()
+    
+    ftime.write('combination time: %f sec\n\n' % time_elapsed)
 
     from time import localtime, strftime
     times = strftime("%Y-%m-%d %H:%M:%S", localtime())
