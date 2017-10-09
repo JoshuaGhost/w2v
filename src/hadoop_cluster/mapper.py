a578d6f103cde168328b2c709083389ff8094320
Joshua Ghost <sniperstriker@gmail.com>
2017-10-09 08:44:51 +0200

    update thesis, write a note that the thesis redirects to overleaf

--- src/hadoop_cluster/mapper.py
+++ src/hadoop_cluster/mapper.py
@@ -4,7 +4,37 @@ from gensim.models.word2vec import LineSentence
 
 import sys
 
-for line in sys.stdin:
-    m = Word2Vec(LineSentence(line.strip()), min_count=0)
+class DividedLineSentence(object):
+    def __init__(self, source, strategy='sampling', npart=10):
+        self.strategy = strategy
+        self.npart = npart
+        self.source = open(source, 'r')
+        if strategy=='skip':
+            self.centences = self.divide_skip(source, npart)
+            self.tempdir='skip/'
+        elif strategy=='sampeling':
+            self.centences = self.divide_bootstrap(source, npart)
+            self.tempdir='sampeling/'
+
+    def __iter__(self):
+        self.source.seek(0)
+        for centence in self.centences:
+            yield centence
+
+    def __del__(self):
+        self.source_doc.close()
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        m = Word2Vec(DividedLieSentence(line.strip()),
+                        size = 500,
+                        negative = 5,
+                        workers = 18,
+                        window = 10,
+                        sg = 1,
+                        null_word = 1,
+                        min_count = new_min_count,
+                        sample = 1e-4)
+
     for word in m.wv.vocab:
         print '\t'.join((word, ','.join([str(num) for num in m.wv[word]])))
--- src/hadoop_cluster/run_all.sh
+++ src/hadoop_cluster/run_all.sh
@@ -1,13 +1,29 @@
-HADOOP_HOME=/opt/cloudera/parcels/CDH/
-HADOOP_STREAM_JAR=${HADOOP_HOME}/jars/hadoop-streaming-2.6.0-mr1-cdh5.12.0.jar
+HOSTAME=$(uname -n)
+if [ $HOSTNAME == "Watchdog" ]; then
+    HADOOP_HOME=/usr/local/hadoop/
+    HADOOP_STREAM_JAR=${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-2.6.5.jar
+else
+    HADOOP_HOME=/opt/cloudera/parcels/CDH/
+    HADOOP_STREAM_JAR=${HADOOP_HOME}/jars/hadoop-streaming-2.6.0-mr1-cdh5.12.0.jar
+fi
 HADOOP_BIN=${HADOOP_HOME}/bin/hadoop
-virtualenv env
-source env/bin/activate
-pip install gensim
-cd env
-zip -r ../env.zip *
-cd ..
-hdfs dfs -rm -r output;
+
+if [ ! -f env.zip ]; then
+    virtualenv env
+    source env/bin/activate
+    pip install gensim
+    cd env
+    zip -r ../env.zip *
+    cd ..
+fi
+
+hdfs dfs -test -e output
+if [ $? -eq 0 ]; then
+    hdfs dfs -rm -r output;
+fi
+
+
+hdfs dfs -put filename.txt
 $HADOOP_BIN jar $HADOOP_STREAM_JAR\
            -archives env.zip#env\
            -file mapper.py\
@@ -18,5 +34,12 @@ $HADOOP_BIN jar $HADOOP_STREAM_JAR\
            -file ./filename.txt\
            -input filename.txt\
            -output output;
-rm -r output;
-hdfs dfs -get output
+
+if [ -d output ]; then
+    rm -r output;
+fi
+
+hdfs dfs -test -e output
+if [$? -eq 0 ]; then
+    hdfs dfs -get output
+fi
