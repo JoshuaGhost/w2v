1540ed12110ccffad67123b536dbc82a28dcca6d
Joahua Ghost <sniperstriker@gmail.com>
2017-12-13 12:50:14 +0100

    reorginize the evaluation code, add concate.py for concatenation experiments

--- src/combine_scripts/concate.py
+++ src/combine_scripts/concate.py
@@ -1,13 +1,15 @@
 from utils import load_embeddings 
 from pickle import dump
 
-folder_in = '/home/zijian/workspace/master_thesis/w2v/temp/models/sampling/sub/'
-filename = 'article.txt.'
-extension = '.txt.w2v'
+#folder_in = '/home/zijian/workspace/master_thesis/w2v/temp/models/sampling/sub/'
+folder_in = '/home/zijian/workspace/master_thesis/w2v/temp/models/documentwise/subs/'
+filename = 'article.txt.0'
+extension = '.w2v'
 nmodels = 10
 norm = False
 mean_corr = False
-folder_out = '/home/zijian/workspace/master_thesis/w2v/temp/models/sampling/combined/'
+#folder_out = '/home/zijian/workspace/master_thesis/w2v/temp/models/sampling/combined/'
+folder_out = '/home/zijian/workspace/master_thesis/w2v/temp/models/documentwise/combined/'
 dump_name = 'concate'
 
 vocab, vecs = load_embeddings(folder_in, filename, extension, nmodels, norm)
