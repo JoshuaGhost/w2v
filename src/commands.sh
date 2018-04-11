270afe54e7077e67f57321a3152d4bf08327e703
Joahua Ghost <sniperstriker@gmail.com>
2018-04-11 13:01:17 +0200

    add frequently used commands to command.sh

---
+++ src/commands.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for dataset in {MEN,RW,Google,WS353}; do python main.py -t bench --dump_folder /tmp/zzj/ --cfolder ../two_10_pct_sub_corpora/ --cname_source c0 --cname_target c1 --vbfolder ../benchmark_vocabs/ --vbname $dataset 1>>stdout 2>>stderr; done
